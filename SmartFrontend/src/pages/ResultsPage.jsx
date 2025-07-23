0235c6c1dfa9904ab11fc56052ec4328eda2cd26
hurtadx <andreshm253@gmail.com>
2025-07-23 04:54:40 -0500

    feat: agregar componente ResultsPage que muestra los resultados de la encuesta

---
+++ SmartFrontend/src/pages/ResultsPage.jsx
@@ -0,0 +1,9 @@
+import { SurveyResults } from '../features/survey/components/SurveyResults';
+
+export const ResultsPage = () => {
+  return (
+    <div className="page-container">
+      <SurveyResults />
+    </div>
+  );
+};
